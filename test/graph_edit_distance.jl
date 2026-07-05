9e52ce4c9405e154fd7d2e089296efb50b44e4c3
Jonas Handwerker <jonas.handwerker@web.de>
2026-07-06 00:35:45 +0200

    Improve formatting in unit tests

--- test/graph_edit_distance.jl
+++ test/graph_edit_distance.jl
@@ -6,7 +6,7 @@ all_formulations = [F1, F1prime, F1plus, F2, F2minus, F2plus, FORI]
 @testset "Small Graph" begin
     G = Graph(3)
     add_edge!(G, 1, 2)
-    
+
     H = Graph(3)
     add_edge!(H, 2, 3)
     add_edge!(H, 1, 3)
@@ -23,12 +23,12 @@ end
 @testset "Cost Functions Simple" begin
     G = Graph(3)
     add_edge!(G, 1, 2)
-    
+
     H = Graph(3)
     add_edge!(H, 2, 3)
     add_edge!(H, 1, 3)
     c_origin = GraphsOptim.get_default_edit_costs(G, H)
-    
+
     # increating costs for node subsitutions 
     c = GraphsOptim.EditCosts(
         ones(Int, nv(G), nv(H)),
@@ -36,7 +36,7 @@ end
         5 * c_origin.c_εk,
         c_origin.c_ijkl,
         c_origin.c_ijε,
-        c_origin.c_εkl
+        c_origin.c_εkl,
     )
     for formulation in all_formulations
         model = Model(HiGHS.Optimizer)
@@ -52,7 +52,7 @@ end
         c_origin.c_εk,
         c_origin.c_ijkl,
         100 * c_origin.c_ijε,
-        10 * c_origin.c_εkl
+        10 * c_origin.c_εkl,
     )
     for formulation in all_formulations
         model = Model(HiGHS.Optimizer)
@@ -65,22 +65,22 @@ end
     # force a specific suboptimal node map with edit costs 
     G = Graph(3)
     add_edge!(G, 2, 3)
-    
+
     H = Graph(4)
     add_edge!(H, 1, 3)
     add_edge!(H, 3, 4)
     c_origin = GraphsOptim.get_default_edit_costs(G, H)
     custom_substitution_cost = 100 .+ c_origin.c_ik
-    custom_substitution_cost[1,1] = 0
-    custom_substitution_cost[2,2] = 0
-    custom_substitution_cost[3,3] = 0
+    custom_substitution_cost[1, 1] = 0
+    custom_substitution_cost[2, 2] = 0
+    custom_substitution_cost[3, 3] = 0
     c = GraphsOptim.EditCosts(
         custom_substitution_cost,
         c_origin.c_iε,
         c_origin.c_εk,
         c_origin.c_ijkl,
         c_origin.c_ijε,
-        c_origin.c_εkl
+        c_origin.c_εkl,
     )
     for formulation in all_formulations
         model = Model(HiGHS.Optimizer)
@@ -88,15 +88,15 @@ end
         set_silent(model)
         optimize!(model)
         @test objective_value(model) == 4
-        @test value(model[:x][1,1]) == 1
-        @test value(model[:x][2,2]) == 1
-        @test value(model[:x][3,3]) == 1
+        @test value(model[:x][1, 1]) == 1
+        @test value(model[:x][2, 2]) == 1
+        @test value(model[:x][3, 3]) == 1
     end
 end
 
 @testset "Edgecases" begin
     G = Graph(3)
-    
+
     H = Graph(3)
     add_edge!(H, 1, 2)
 
@@ -125,7 +125,7 @@ end
         optimize!(model)
         @test objective_value(model) == 2
     end
-    
+
     # no nodes in one graph
     H = Graph(0)
     for formulation in [F1, F1prime, F1plus]
